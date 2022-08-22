1b6c54243a8993c3ba35b8474e01aad535765831
AbdManian <abd.manian@gmail.com>
2022-08-22 10:15:12 +0430

    Initial implementation for argpring

--- 01-argprint/argprint.cpp
+++ 01-argprint/argprint.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-    /* code */
+    for (int i = 0; i < argc; i++)
+    {
+        std::cout << "Arg["<<i+1<<"] = " << argv[i] << '\n';
+    }
+    
     return 0;
 }
