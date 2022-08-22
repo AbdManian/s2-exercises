f1afa35c471c2955ce32af34e39f5aa72cd25e89
AbdManian <abd.manian@gmail.com>
2022-08-22 10:18:34 +0430

    Reformat the cpp code

--- 01-argprint/argprint.cpp
+++ 01-argprint/argprint.cpp
@@ -4,8 +4,8 @@ int main(int argc, char *argv[])
 {
     for (int i = 0; i < argc; i++)
     {
-        std::cout << "Arg["<<i+1<<"] = " << argv[i] << '\n';
+        std::cout << "Arg[" << i + 1 << "] = " << argv[i] << '\n';
     }
-    
+
     return 0;
 }
